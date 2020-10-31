c442678702bea8f1fbb53f857da3f602834ead55
maidiyangsa <57142580+maidiyangsa@users.noreply.github.com>
2020-10-31 19:02:32 +0800

    Add files via upload

---
+++ Kelipatan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kelipatan;
+/**
+ *
+ * @author Beta Three
+ * NAMA         : Adrian Maidiyangsa
+ * KELAS        : IF-1
+ * NIM          : 10119029
+ * Deskripsi Program : Program ini berisi program kelipatan angka, program ini dibuat sendiri
+ * dan dibantu oleh sumber-sumber yang ada diinternet
+ */
+public class Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        double i;
+        for(i=3.5; i<=35; i=i+3.5){
+            System.out.println(i);
+        }
+    }
+    
+}
